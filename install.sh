76629a8c818c28867d342b093b1f193b73e7a8e9
Anand Sampat <anands@cs.stanford.edu>
2016-03-05 17:52:59 -0800

    removed check for certificate when dling anaconda

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ sudo apt-get --yes --force-yes install libgflags-dev libgoogle-glog-dev liblmdb-
 #######################################################
 
 # install anaconda python
-wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
+wget --no-check-certificate https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh
 
 bash Anaconda-2.3.0-Linux-x86_64.sh -b
 
